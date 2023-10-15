9c72a42fa8b300e03884bd5f9c58852e6126c6af
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-10-16 00:03:17 +0500

    Task 7

---
+++ Tyuiu.ElmurzaevIH.Sprint2.Task6.V2.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ElmurzaevIH.Sprint2.Task6.V2.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tyuiu.ElmurzaevIH.Sprint2.Task7.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElmurzaevIH.Sprint2.Task7.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Tyuiu.ElmurzaevIH.Sprint2.Task7.V11.Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6925592-7935-4be3-a35a-7dfbc9a0ea04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
